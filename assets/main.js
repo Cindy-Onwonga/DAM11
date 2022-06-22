bc2348409b320803c2e6ac1c5302c33ceb6563f6
Cindy Kerubo <cindykerubo@gmail.com>
2022-06-22 22:31:06 +0000

    did assignment 1

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,8 @@
+let username = getElementById("name")
+
+//console.log("Hello" + username + "welcome to the bootcamp")
+
+function nameFunc() {
+  getElementById("displayname").innerHTML = "Hello" + username + "welcome to the bootcamp"
+}
 
